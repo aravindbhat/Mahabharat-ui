1389b593ead4fa0c81ab0e00e7685a785a743e24
aravind <aravindcbk@gmail.com>
2025-11-18 12:03:48 +0530

    learning  event

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,39 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  //lamo="";
+  a=10;
+  b=20;
+  sum=0;
   protected readonly title = signal('Mahabharat-ui');
+  clickEvent(){
+    console.log("clicked");
+    
+
+  }
+  mouseEnterEvent(){
+    console.log("Mouse entered");
+    
+  }
+  mouseLeaveEvent(){
+    console.log("Mouse left");
+    
+  }
+  add(x:string, y:string){
+    this.sum=parseInt(x)+parseInt(y);
+  }
+  assignA(valA:HTMLInputElement){
+    this.a=parseInt(valA.value);
+    console.log("A",this.a);
+    
+  }
+  assignB(valB:HTMLInputElement){
+    this.b=parseInt(valB.value);
+    console.log("B", this.b);
+    
+  }
+  reset(){
+    this.sum=this.a=this.b=0;
+    
+  }
 }
